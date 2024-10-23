051b69ee900055eb4126b17dd13d86db04d477a1
JulianOjea <32637786+JulianOjea@users.noreply.github.com>
2024-10-23 16:00:17 +0200

    Add app routes not found

--- triptolemus-web-admin/src/app/app.routes.ts
+++ triptolemus-web-admin/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from './auth.guard';
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'question', component: QuestionComponent, canActivate: [AuthGuard] },
+    { path: 'not-found', component: NotFoundComponent },
     { path: '',   redirectTo: '/login', pathMatch: 'full' },
     { path: '**', component: NotFoundComponent }
 ];
